752c264485ab9e3a2d42239a4f3e99159e0fb045
Boki23m <boki23usa@yahoo.com>
2019-05-13 18:18:37 -0700

    new front end added

---
+++ client/src/pages/Booking.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Booking extends Component {
+  render() {
+    return (
+        <div>
+          <h2>Booking</h2>
+        </div>
+    );
+  }
+}
+
+export default Booking;
