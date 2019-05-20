56f0d980768081c109ec2024c0c36bbf91d0883e
Boki23m <boki23usa@yahoo.com>
2019-05-20 13:57:27 -0700

    my trips added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import PrivateRoute from './components/PrivateRoute';
+// import PrivateRoute from './components/PrivateRoute';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Booking from './pages/Booking';
@@ -20,13 +20,12 @@ function App() {
       <Navbar />
       <Switch>
         <Route exact path='/' component={Home} />
-        <Route exact path='/' component={Home} />
         <Route exact path='/contact' component={Contact} />
         <Route exact path='/booking' component={Booking} />
         <Route exact path='/faqs' component={Faqs} />
         <Route exact path='/signin' component={SignIn} />
         <Route exact path='/signup' component={SignUp} />
-        <PrivateRoute exact path='/mytrips' component={MyTrips} />
+        <Route exact path='/mytrips' component={MyTrips} />
         <Route exact path='/terms' component={Terms} />
         <Route component={NoMatch} />
       </Switch>
--- client/src/pages/MyTrips.js
+++ client/src/pages/MyTrips.js
@@ -1,32 +1,115 @@
-import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import withStyles from '@material-ui/core/styles/withStyles';
 
-const useStyles = makeStyles(theme => ({
+
+const styles = theme => ({
     '@global': {
-        strong: {
-            fontWeight: theme.typography.fontWeightMedium,
+        body: {
+            backgroundColor: theme.palette.common.white,
         },
     },
-    container: {
+    paper: {
         marginTop: theme.spacing(8),
-        marginBottom: theme.spacing(8),
-        position: 'relative',
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
     },
-}));
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+});
 
-export default function MyTrips() {
-    const classes = useStyles();
-
-    return (
-        <Container className={classes.container}>
-            <Typography variant="h4" align="center" color="textPrimary" component="p">
-            My Trips section (coming soon...)
-            </Typography>
-        </Container>
-    );
+class MyTrips extends React.Component {
+    state = {
+        reservationNo: "",
+        email: "",
+    };
+    render() {
+        const { classes } = this.props;
+        return (
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <div className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5" gutterBottom>
+                        Manage Your Trip
+        </Typography>
+                    <Typography component="h1" align='center' variant="h6">
+                        To manage an existing reservation we need your 7 digit confirmation number and the last name.
+        </Typography>
+                    <form className={classes.form} noValidate>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="confirmation"
+                            label="Confirmation Number"
+                            value={this.state.reservationNo}
+                            onChange={this.handleInputChange}
+                            name="confirmation"
+                            autoComplete="number"
+                            autoFocus
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="email"
+                            label="Email Address"
+                            type="email"
+                            id="email"
+                            value={this.state.email}
+                            onChange={this.handleInputChange}
+                            autoComplete="email"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                            disabled={!(this.state.reservationNo && this.state.email)}
+                            onClick={this.handleFormSubmit}
+                        >
+                            Find Trip
+          </Button>
+          <Typography align='center' variant="body1">
+                        Please Note: If you have a round trip reservation, you need to only enter one of the confirmation numbers to manage both legs of the trip.
+                        </Typography>
+        </form>
+                </div>
+                <Box mt={5}>
+                    <Typography variant="body2" color="textSecondary" align="center">
+                        {'Developed by Bojan & Veselin.'}
+                    </Typography>
+                </Box>
+            </Container>
+        )
+    }
 }
+
+export default withStyles(styles)(MyTrips);
